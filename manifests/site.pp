24d2aeb27a418ac6621f28aac789c8326fe1d38d
Kim jeong_guk <mindstar7949@naver.com>
2019-05-13 11:19:20 +0900

    Removed file

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,9 +5,6 @@ file {'/tmp/HelloPuppetMaster':
 	content => "Hello~~~~~~\n",
 }
 node 'vm01.linuxmaster.com'{
-	file { '/tmp/testserver':
-		content => this is a test server.\n",
-	}
 	include sshd
 	include vim_upgrade
 
